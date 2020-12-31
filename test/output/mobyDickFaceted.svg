LordOfCodes26/plot

commit c1681c1460c0b84ac92212b2f3713d838a362e50

fix facet order

src/marks/facet.js
@@ -1,4 +1,4 @@
-import {group, groups} from "d3-array";
+import {cross, group, groups} from "d3-array";
 import {create} from "d3-selection";
 import {Mark} from "../mark.js";
 import {autoScaleRange} from "../scales.js";
@@ -58,7 +58,7 @@ class Facet extends Mark {
 
     return create("svg:g")
         .call(g => g.selectAll()
-          .data(facets.keys())
+          .data(facetKeys(scales).filter(key => facets.has(key)))
           .join("g")
             .attr("transform", facetTranslate(fx, fy))
             .each(function(key) {
@@ -81,6 +81,12 @@ export function facets(data, {x, y, ...options}, marks) {
   return x === undefined && y === undefined
     ? marks // if no facets are specified, ignore!
     : [new Facet(data, {x, y, ...options}, marks)];
+}
+
+function facetKeys({fx, fy}) {
+  return fx && fy ? cross(fx.domain(), fy.domain())
+    : fx ? fx.domain()
+    : fy.domain();
 }
 
 function facetGroups(index, channels) {

test/output/ballotStatusRace.svg
@@ -49,6 +49,38 @@
     </g><text fill="currentColor" transform="translate(640,30)" dy="-0.32em" text-anchor="end">Frequency (%) →</text>
   </g>
   <g>
+    <g transform="translate(0,27)">
+      <g transform="translate(0,0)">
+        <rect x="210" width="410" y="2" height="11" fill="currentColor">
+          <title>79.0%</title>
+        </rect>
+        <rect x="210" width="2.1016100611808213" y="26" height="11" fill="gray">
+          <title>0.4%</title>
+        </rect>
+        <rect x="210" width="106.57438875906558" y="14" height="11" fill="brown">
+          <title>20.5%</title>
+        </rect>
+      </g>
+      <g stroke="currentColor" transform="translate(0.5,0)">
+        <line x1="210" x2="210" y1="0" y2="38"></line>
+      </g>
+    </g>
+    <g transform="translate(0,69)">
+      <g transform="translate(0,0)">
+        <rect x="210" width="405.8914002270852" y="2" height="11" fill="currentColor">
+          <title>78.3%</title>
+        </rect>
+        <rect x="210" width="3.1538887637774167" y="26" height="11" fill="gray">
+          <title>0.6%</title>
+        </rect>
+        <rect x="210" width="109.6307098293837" y="14" height="11" fill="brown">
+          <title>21.1%</title>
+        </rect>
+      </g>
+      <g stroke="currentColor" transform="translate(0.5,0)">
+        <line x1="210" x2="210" y1="0" y2="38"></line>
+      </g>
+    </g>
     <g transform="translate(0,111)">
       <g transform="translate(0,0)">
         <rect x="210" width="404.1524922619212" y="2" height="11" fill="currentColor">
@@ -59,38 +91,6 @@
         </rect>
         <rect x="210" width="3.191901653376277" y="26" height="11" fill="gray">
           <title>0.6%</title>
-        </rect>
-      </g>
-      <g stroke="currentColor" transform="translate(0.5,0)">
-        <line x1="210" x2="210" y1="0" y2="38"></line>
-      </g>
-    </g>
-    <g transform="translate(0,279)">
-      <g transform="translate(0,0)">
-        <rect x="210" width="349.7813163559978" y="2" height="11" fill="currentColor">
-          <title>67.4%</title>
-        </rect>
-        <rect x="210" width="163.32285937333586" y="14" height="11" fill="brown">
-          <title>31.5%</title>
-        </rect>
-        <rect x="210" width="5.571823090912687" y="26" height="11" fill="gray">
-          <title>1.1%</title>
-        </rect>
-      </g>
-      <g stroke="currentColor" transform="translate(0.5,0)">
-        <line x1="210" x2="210" y1="0" y2="38"></line>
-      </g>
-    </g>
-    <g transform="translate(0,237)">
-      <g transform="translate(0,0)">
-        <rect x="210" width="132.02293626031644" y="14" height="11" fill="brown">
-          <title>25.5%</title>
-        </rect>
-        <rect x="210" width="374.6049571803459" y="2" height="11" fill="currentColor">
-          <title>72.2%</title>
-        </rect>
-        <rect x="210" width="12.048105379584086" y="26" height="11" fill="gray">
-          <title>2.3%</title>
         </rect>
       </g>
       <g stroke="currentColor" transform="translate(0.5,0)">
@@ -129,32 +129,32 @@
         <line x1="210" x2="210" y1="0" y2="38"></line>
       </g>
     </g>
-    <g transform="translate(0,69)">
+    <g transform="translate(0,237)">
       <g transform="translate(0,0)">
-        <rect x="210" width="405.8914002270852" y="2" height="11" fill="currentColor">
-          <title>78.3%</title>
+        <rect x="210" width="132.02293626031644" y="14" height="11" fill="brown">
+          <title>25.5%</title>
         </rect>
-        <rect x="210" width="3.1538887637774167" y="26" height="11" fill="gray">
-          <title>0.6%</title>
+        <rect x="210" width="374.6049571803459" y="2" height="11" fill="currentColor">
+          <title>72.2%</title>
         </rect>
-        <rect x="210" width="109.6307098293837" y="14" height="11" fill="brown">
-          <title>21.1%</title>
+        <rect x="210" width="12.048105379584086" y="26" height="11" fill="gray">
+          <title>2.3%</title>
         </rect>
       </g>
       <g stroke="currentColor" transform="translate(0.5,0)">
         <line x1="210" x2="210" y1="0" y2="38"></line>
       </g>
     </g>
-    <g transform="translate(0,27)">
+    <g transform="translate(0,279)">
       <g transform="translate(0,0)">
-        <rect x="210" width="410" y="2" height="11" fill="currentColor">
-          <title>79.0%</title>
+        <rect x="210" width="349.7813163559978" y="2" height="11" fill="currentColor">
+          <title>67.4%</title>
         </rect>
-        <rect x="210" width="2.1016100611808213" y="26" height="11" fill="gray">
-          <title>0.4%</title>
+        <rect x="210" width="163.32285937333586" y="14" height="11" fill="brown">
+          <title>31.5%</title>
         </rect>
-        <rect x="210" width="106.57438875906558" y="14" height="11" fill="brown">
-          <title>20.5%</title>
+        <rect x="210" width="5.571823090912687" y="26" height="11" fill="gray">
+          <title>1.1%</title>
         </rect>
       </g>
       <g stroke="currentColor" transform="translate(0.5,0)">

test/output/penguinMassSex.svg
@@ -45,16 +45,16 @@
     </g><text fill="currentColor" transform="translate(640,30)" dy="-0.32em" text-anchor="end">Body mass (g) →</text>
   </g>
   <g>
-    <g transform="translate(0,254)">
+    <g transform="translate(0,32)">
       <g transform="translate(0,0)">
-        <rect x="71" y="100" width="68" height="0"></rect>
-        <rect x="140" y="86.79245283018868" width="68" height="13.20754716981132"></rect>
-        <rect x="209" y="11.32075471698113" width="67" height="88.67924528301887"></rect>
-        <rect x="277" y="20.75471698113207" width="68" height="79.24528301886792"></rect>
-        <rect x="346" y="75.47169811320755" width="68" height="24.52830188679245"></rect>
-        <rect x="415" y="50.943396226415096" width="68" height="49.056603773584904"></rect>
-        <rect x="484" y="45.28301886792453" width="67" height="54.71698113207547"></rect>
-        <rect x="552" y="92.45283018867924" width="68" height="7.547169811320757"></rect>
+        <rect x="71" y="98.11320754716981" width="68" height="1.8867924528301927"></rect>
+        <rect x="140" y="96.22641509433963" width="68" height="3.773584905660371"></rect>
+        <rect x="209" y="98.11320754716981" width="67" height="1.8867924528301927"></rect>
+        <rect x="277" y="96.22641509433963" width="68" height="3.773584905660371"></rect>
+        <rect x="346" y="94.33962264150944" width="68" height="5.660377358490564"></rect>
+        <rect x="415" y="100" width="68" height="0"></rect>
+        <rect x="484" y="100" width="67" height="0"></rect>
+        <rect x="552" y="100" width="68" height="0"></rect>
       </g>
       <g stroke="currentColor" transform="translate(0,0.5)">
         <line x1="70" x2="620" y1="100" y2="100"></line>
@@ -75,16 +75,16 @@
         <line x1="70" x2="620" y1="100" y2="100"></line>
       </g>
     </g>
-    <g transform="translate(0,32)">
+    <g transform="translate(0,254)">
       <g transform="translate(0,0)">
-        <rect x="71" y="98.11320754716981" width="68" height="1.8867924528301927"></rect>
-        <rect x="140" y="96.22641509433963" width="68" height="3.773584905660371"></rect>
-        <rect x="209" y="98.11320754716981" width="67" height="1.8867924528301927"></rect>
-        <rect x="277" y="96.22641509433963" width="68" height="3.773584905660371"></rect>
-        <rect x="346" y="94.33962264150944" width="68" height="5.660377358490564"></rect>
-        <rect x="415" y="100" width="68" height="0"></rect>
-        <rect x="484" y="100" width="67" height="0"></rect>
-        <rect x="552" y="100" width="68" height="0"></rect>
+        <rect x="71" y="100" width="68" height="0"></rect>
+        <rect x="140" y="86.79245283018868" width="68" height="13.20754716981132"></rect>
+        <rect x="209" y="11.32075471698113" width="67" height="88.67924528301887"></rect>
+        <rect x="277" y="20.75471698113207" width="68" height="79.24528301886792"></rect>
+        <rect x="346" y="75.47169811320755" width="68" height="24.52830188679245"></rect>
+        <rect x="415" y="50.943396226415096" width="68" height="49.056603773584904"></rect>
+        <rect x="484" y="45.28301886792453" width="67" height="54.71698113207547"></rect>
+        <rect x="552" y="92.45283018867924" width="68" height="7.547169811320757"></rect>
       </g>
       <g stroke="currentColor" transform="translate(0,0.5)">
         <line x1="70" x2="620" y1="100" y2="100"></line>

commit d032cebfea41511a0e772e366b1cb956702beec1

fixes the x/y faceting
it got broken after c1681c1460c0b84ac92212b2f3713d838a362e50 since the [x,y] we are trying to match are not the same arrays (thought they contain the same data).

(Fixing with a JSON.stringify'd key is probably not the most elegant solution.)

src/marks/facet.js
@@ -36,7 +36,7 @@ class Facet extends Mark {
         markIndex.set(mark, index);
         markChannels.set(mark, named);
       }
-      facets.set(facetKey, {markIndex, markChannels});
+      facets.set(JSON.stringify(facetKey), {markIndex, markChannels});
     }
     return {index, channels: [...channels, ...subchannels]};
   }
@@ -58,11 +58,11 @@ class Facet extends Mark {
 
     return create("svg:g")
         .call(g => g.selectAll()
-          .data(facetKeys(scales).filter(key => facets.has(key)))
+          .data(facetKeys(scales).filter(key => facets.has(JSON.stringify(key))))
           .join("g")
             .attr("transform", facetTranslate(fx, fy))
             .each(function(key) {
-              const {markIndex, markChannels} = facets.get(key);
+              const {markIndex, markChannels} = facets.get(JSON.stringify(key));
               for (const mark of marks) {
                 const node = mark.render(
                   markIndex.get(mark),

test/output/mobyDickFaceted.svg
@@ -1,0 +1,101 @@
+<svg viewBox="0,0,640,396" class="plot" fill="currentColor" stroke-miterlimit="1" text-anchor="middle" font-size="10" font-family="sans-serif" style="max-width: 640px; display: block; background: white;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <style>
+    .plot text {
+      white-space: pre;
+    }
+  </style>
+  <g transform="translate(40,0)" fill="none" text-anchor="end">
+    <g class="tick" opacity="1" transform="translate(0,111.5)">
+      <line stroke="currentColor" x2="-6"></line><text fill="currentColor" x="-9" dy="0.32em">lower</text>
+    </g>
+    <g class="tick" opacity="1" transform="translate(0,275.5)">
+      <line stroke="currentColor" x2="-6"></line><text fill="currentColor" x="-9" dy="0.32em">upper</text>
+    </g>
+  </g>
+  <g transform="translate(0,366)" fill="none" text-anchor="middle">
+    <g class="tick" opacity="1" transform="translate(192.5,0)">
+      <line stroke="currentColor" y2="6"></line><text fill="currentColor" y="9" dy="0.71em"></text>
+    </g>
+    <g class="tick" opacity="1" transform="translate(468.5,0)">
+      <line stroke="currentColor" y2="6"></line><text fill="currentColor" y="9" dy="0.71em">vowel</text>
+    </g>
+  </g>
+  <g>
+    <g transform="translate(68,37)">
+      <g transform="translate(0,0)">
+        <rect x="106" width="8" y="96.37209302325581" height="51.627906976744185"></rect>
+        <rect x="115" width="8" y="115.79401993355481" height="32.20598006644519"></rect>
+        <rect x="169" width="8" y="65.27242524916943" height="82.72757475083057"></rect>
+        <rect x="70" width="8" y="80.63787375415282" height="67.36212624584718"></rect>
+        <rect x="160" width="8" y="82.6046511627907" height="65.3953488372093"></rect>
+        <rect x="61" width="8" y="122.30897009966776" height="25.69102990033224"></rect>
+        <rect x="124" width="8" y="69.69767441860465" height="78.30232558139535"></rect>
+        <rect x="196" width="8" y="135.46179401993354" height="12.538205980066465"></rect>
+        <rect x="34" width="8" y="106.08305647840531" height="41.91694352159469"></rect>
+        <rect x="205" width="8" y="125.13621262458473" height="22.86378737541527"></rect>
+        <rect x="142" width="8" y="125.62790697674419" height="22.372093023255815"></rect>
+        <rect x="25" width="8" y="122.43189368770764" height="25.568106312292358"></rect>
+        <rect x="178" width="8" y="41.671096345514954" height="106.32890365448505"></rect>
+        <rect x="16" width="8" y="132.75747508305648" height="15.242524916943523"></rect>
+        <rect x="52" width="8" y="123.29235880398672" height="24.707641196013284"></rect>
+        <rect x="232" width="8" y="147.13953488372093" height="0.8604651162790731"></rect>
+        <rect x="151" width="8" y="146.8936877076412" height="1.1063122923588082"></rect>
+        <rect x="97" width="8" y="141.2392026578073" height="6.760797342192689"></rect>
+        <rect x="214" width="8" y="146.64784053156146" height="1.3521594684385434"></rect>
+        <rect x="88" width="8" y="146.8936877076412" height="1.1063122923588082"></rect>
+      </g>
+      <g stroke="currentColor" transform="translate(0,0.5)">
+        <line x1="0" x2="248" y1="148" y2="148"></line>
+      </g>
+    </g>
+    <g transform="translate(68,201)">
+      <g transform="translate(0,0)">
+        <rect x="25" width="8" y="146.5249169435216" height="1.475083056478411"></rect>
+        <rect x="169" width="8" y="146.156146179402" height="1.8438538205980137"></rect>
+        <rect x="205" width="8" y="145.66445182724254" height="2.3355481727574556"></rect>
+        <rect x="124" width="8" y="146.27906976744185" height="1.7209302325581461"></rect>
+        <rect x="178" width="8" y="145.41860465116278" height="2.581395348837219"></rect>
+        <rect x="115" width="8" y="147.6312292358804" height="0.36877076411960275"></rect>
+        <rect x="160" width="8" y="147.50830564784053" height="0.4916943521594703"></rect>
+        <rect x="106" width="8" y="147.2624584717608" height="0.7375415282392055"></rect>
+        <rect x="61" width="8" y="147.01661129568106" height="0.9833887043189407"></rect>
+        <rect x="70" width="8" y="147.01661129568106" height="0.9833887043189407"></rect>
+        <rect x="142" width="8" y="147.13953488372093" height="0.8604651162790731"></rect>
+        <rect x="16" width="8" y="146.27906976744185" height="1.7209302325581461"></rect>
+        <rect x="34" width="8" y="147.6312292358804" height="0.36877076411960275"></rect>
+        <rect x="88" width="8" y="147.75415282392026" height="0.24584717607973516"></rect>
+        <rect x="52" width="8" y="147.01661129568106" height="0.9833887043189407"></rect>
+        <rect x="196" width="8" y="147.75415282392026" height="0.24584717607973516"></rect>
+      </g>
+      <g stroke="currentColor" transform="translate(0,0.5)">
+        <line x1="0" x2="248" y1="148" y2="148"></line>
+      </g>
+    </g>
+    <g transform="translate(344,37)">
+      <g transform="translate(0,0)">
+        <rect x="7" width="8" y="52.48837209302325" height="95.51162790697674"></rect>
+        <rect x="43" width="8" y="0" height="148"></rect>
+        <rect x="133" width="8" y="57.03654485049834" height="90.96345514950167"></rect>
+        <rect x="223" width="8" y="122.67774086378738" height="25.322259136212622"></rect>
+        <rect x="79" width="8" y="72.77076411960132" height="75.22923588039868"></rect>
+        <rect x="187" width="8" y="115.91694352159469" height="32.08305647840531"></rect>
+      </g>
+      <g stroke="currentColor" transform="translate(0,0.5)">
+        <line x1="0" x2="248" y1="148" y2="148"></line>
+      </g>
+    </g>
+    <g transform="translate(344,201)">
+      <g transform="translate(0,0)">
+        <rect x="79" width="8" y="140.74750830564784" height="7.252491694352159"></rect>
+        <rect x="7" width="8" y="145.78737541528238" height="2.2126245847176165"></rect>
+        <rect x="223" width="8" y="147.38538205980066" height="0.6146179401993379"></rect>
+        <rect x="133" width="8" y="147.2624584717608" height="0.7375415282392055"></rect>
+        <rect x="43" width="8" y="147.2624584717608" height="0.7375415282392055"></rect>
+        <rect x="187" width="8" y="147.87707641196013" height="0.12292358803986758"></rect>
+      </g>
+      <g stroke="currentColor" transform="translate(0,0.5)">
+        <line x1="0" x2="248" y1="148" y2="148"></line>
+      </g>
+    </g>
+  </g>
+</svg>

test/plots/index.js
@@ -22,6 +22,7 @@ export {default as metroUnemploymentRidgeline} from "./metro-unemployment-ridgel
 export {default as metroUnemployment} from "./metro-unemployment.js";
 export {default as mobyDickLetterFrequency} from "./moby-dick-letter-frequency.js";
 export {default as mobyDickLetterPosition} from "./moby-dick-letter-position.js";
+export {default as mobyDickFaceted} from "./moby-dick-faceted.js";
 export {default as morleyBoxplot} from "./morley-boxplot.js";
 export {default as moviesProfitByGenre} from "./movies-profit-by-genre.js";
 export {default as penguinMassSex} from "./penguin-mass-sex.js";

test/plots/moby-dick-faceted.js
@@ -1,0 +1,40 @@
+import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
+
+export default async function() {
+  const mobydick = await d3.text("data/moby-dick-chapter-1.txt");
+  const letters = [...mobydick].filter(d => /\w/.test(d));
+  const moby = {
+    letters,
+    case: letters.map(d => d.toLowerCase() === d ? "lower" : "upper"),
+    vowel: letters.map(d => /[aeiouy]/i.test(d) ? "vowel" : "")
+  };
+  
+  const defA = {
+    facet: {
+      data: moby.letters,
+      x: moby.vowel,
+      y: moby.case
+    },
+    marks: [
+      Plot.groupX(moby.letters, { x: d => d.toLowerCase() }),
+      Plot.ruleY([0])
+    ]
+  };
+
+/*  todo cf #50
+  const defB = {
+    facet: {
+      data: moby.letters,
+      x: moby.vowel,
+      y: moby.case
+    },
+    marks: [
+      Plot.groupX(moby.letters, {x: moby.letters.map(d => d.toLowerCase())}),
+      Plot.ruleY([0])
+    ]
+  };
+*/
+  
+  return Plot.plot(defA);
+}